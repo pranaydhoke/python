a7cd91c787c502f2e139213e314589b897c3c29a
pranay <pranaydhoke20@gmail.com>
2020-08-25 12:11:53 +0530

    test

---
+++ testAPI.py
@@ -0,0 +1,29 @@
+from flask import Flask, url_for  
+from markupsafe import escape
+
+
+app=Flask(__name__)
+
+@app.route('/pranay')
+def helloWorld():
+    return "Welcome to flask API"
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+
+@app.route('/login')
+def login():
+    return "login"
+
+@app.route('/user/username')
+def profile(username):
+    return "{}\ s profile".format(escape(username))
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='Pranay Dhoke'))
