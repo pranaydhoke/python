d39128df9932f5298b9b972181ee3cd47c7f525f
pranay <pranaydhoke20@gmail.com>
2020-08-25 19:53:34 +0530

    changes

--- FlaskAPIRequest.py
+++ FlaskAPIRequest.py
@@ -1,17 +1,31 @@
 __author__      = "Pranay Dhoke"
-__copyright__   = "Technospace"
+__copyright__   = ""
 
-from flask import request
 from flask import Flask
+from flask import request
+from flask import Flask,flash, redirect, render_template, request, session, abort
+import os
 
 app=Flask(__name__)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        return do_Login()
+        if valid_login(request.form['username'] == 'Admin',request.form['password'] == 'password'):
+                return log_the_user_in(request.form['username'])
+
+        else:
+            error ='Invalid username'
     else:
-        return show_the_login_form()    
+         return render_template('login.html')
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
 
 def show_the_login_form():
     return "Successfully login"
