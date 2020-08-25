9904c44ed73799a9ada1f3a78f58939ab635e1cd
pranay <pranaydhoke20@gmail.com>
2020-08-25 13:31:46 +0530

    GET POST method

---
+++ FlaskAPIRequest.py
@@ -0,0 +1,18 @@
+from flask import request
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return do_Login()
+    else:
+        return show_the_login_form()    
+
+def show_the_login_form():
+    return "Successfully login"
+
+def do_Login():
+    return "login"
+
